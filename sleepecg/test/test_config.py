f22fa0d5e4ca01f8e06e3da3b84486bae9b79bc6
hofaflo <hofaflo@gmail.com>
2021-11-23 14:10:47 +0100

    explain temp_test_config fixture in comments

--- sleepecg/test/test_config.py
+++ sleepecg/test/test_config.py
@@ -13,11 +13,14 @@ from sleepecg import get_config, set_config
 @pytest.fixture(autouse=True)
 def temp_test_config(tmp_path):
     """Create, use and delete a temporary user config file for testing."""
+    # setup
     user_config_path_backup = sleepecg.config._USER_CONFIG_PATH
     sleepecg.config._USER_CONFIG_PATH = tmp_path / 'testconfig.yml'
 
+    # execute test
     yield
 
+    # cleanup
     if sleepecg.config._USER_CONFIG_PATH.is_file():
         sleepecg.config._USER_CONFIG_PATH.unlink()
     sleepecg.config._USER_CONFIG_PATH = user_config_path_backup
