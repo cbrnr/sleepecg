05986668166fa4582146858a74e6bdce108abba0
hofaflo <hofaflo@gmail.com>
2021-11-29 11:52:07 +0100

    update feature extraction example to work with current api [skip ci]

--- examples/feature_extraction.py
+++ examples/feature_extraction.py
@@ -2,21 +2,21 @@
 import numpy as np
 
 from sleepecg import extract_features
-from sleepecg.io import SleepRecord
+from sleepecg.io.sleep_readers import SleepRecord
 
 # Generate dummy data while we don't have reader functions for sleep data
 recording_hours = 8
 heartbeat_times = np.cumsum(np.random.uniform(0.5, 1.5, recording_hours * 3600))
 sleep_stages = np.random.randint(1, 6, int(max(heartbeat_times)) // 30)
-fs_sleep_stages = 1/30
+sleep_stage_duration = 30
 
 rec = SleepRecord(
-    heartbeat_times=heartbeat_times,
     sleep_stages=sleep_stages,
-    fs_sleep_stages=fs_sleep_stages,
+    sleep_stage_duration=sleep_stage_duration,
+    heartbeat_times=heartbeat_times,
 )
 
-features, names = extract_features(
+features, stages, feature_ids = extract_features(
         [rec],
         lookback=30,
         lookforward=90,
