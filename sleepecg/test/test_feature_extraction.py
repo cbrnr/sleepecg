5dcf4b61295838547c4877dd2ce890b273b05061
hofaflo <hofaflo@gmail.com>
2021-12-01 14:30:28 +0100

    avoid win32 memory issues by using shorter data

--- sleepecg/test/test_feature_extraction.py
+++ sleepecg/test/test_feature_extraction.py
@@ -22,7 +22,7 @@ def test_feature_ids():
     the relevant function. Note that the test only compares lengths, so the
     order might still be incorrect.
     """
-    heartbeat_times = np.cumsum(np.random.uniform(0.5, 1.5, 60*60*8))
+    heartbeat_times = np.cumsum(np.random.uniform(0.5, 1.5, 60*60))
     sleep_stages = np.random.randint(1, 6, int(max(heartbeat_times))//30)
     sleep_stage_duration = 30
     rri = np.diff(heartbeat_times)
@@ -38,14 +38,17 @@ def test_feature_ids():
     )
     assert X_time.shape[1] == len(_FEATURE_GROUPS['hrv-time'])
 
+    # The analysis window (i.e. the sum of lookback and lookforward) must
+    # be at least 3030.3 seconds long to give useful PSD estimates in all
+    # frequency ranges, otherwise a warning is issued.
     X_frequency = _hrv_frequencydomain_features(
         rri,
         rri_times,
         stage_times,
-        lookback=0,
+        lookback=3001,
         lookforward=30,
         fs_rri_resample=4,
         max_nans=0,
-        feature_ids=[],  # to avoid any warnings about too short analysis windows
+        feature_ids=_FEATURE_GROUPS['hrv-frequency'],
     )
     assert X_frequency.shape[1] == len(_FEATURE_GROUPS['hrv-frequency'])
