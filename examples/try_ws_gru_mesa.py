ac8bded8aebf613602a3ea772ebae039e67b1fb2
hofaflo <hofaflo@gmail.com>
2022-02-05 18:53:13 +0100

    add tf warnings note [skip ci]

--- examples/try_ws_gru_mesa.py
+++ examples/try_ws_gru_mesa.py
@@ -1,6 +1,8 @@
 # %%
 from sleepecg import load_classifier, plot_hypnogram, read_slpdb, stage
 
+# The model was built using tensorflow 2.7, running on higher versions
+# might create warnings but should not influence the results.
 clf = load_classifier('ws-gru-mesa', 'SleepECG')
 
 # %% Load record
