3e816c1787f472cb3f2758d30ad2ae900df137ef
Clemens Brunner <clemens.brunner@gmail.com>
2023-05-03 10:47:20 +0200

    Fix Black again

--- sleepecg/classification.py
+++ sleepecg/classification.py
@@ -296,7 +296,7 @@ def load_classifier(
 
         if classifier_info["model_type"] == "keras":
             import os
-            
+
             environ_orig = os.environ.copy()
             if silence_tf_messages:
                 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
