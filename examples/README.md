cf8ceee6f47924e3130d1a9a1551142fa2b6c8e1
Clemens Brunner <clemens.brunner@gmail.com>
2023-09-20 13:54:53 +0200

    Add example for staging a custom dataset

--- examples/README.md
+++ examples/README.md
@@ -1,9 +1,15 @@
 # Examples
 
 To run the provided examples, download or clone the [GitHub Repository](https://github.com/cbrnr/sleepecg) and execute the scripts in this directory or its subdirectories.
+
 - Heartbeat detection demo:
     ```
     sleepecg/examples> python heartbeat_detection.py
+    ```
+
+- Classify sleep stages in your own dataset (starting with ECG recording for the sleep duration stored in `sleep.edf` in this example):
+    ```
+    sleepecg/examples> python custom_dataset.py
     ```
 
 - Benchmark heartbeat detector runtime (more info [here](https://github.com/cbrnr/sleepecg/tree/main/examples/benchmark)):
---
+++ examples/custom_dataset.py
@@ -0,0 +1,41 @@
+# %% imports (this example requires mne and tensorflow packages)
+from datetime import datetime, timezone
+
+from mne.io import read_raw_edf
+import sleepecg
+
+# %% load dataset sleep.edf
+raw = read_raw_edf("sleep.edf", include="ECG")
+raw.set_channel_types({"ECG": "ecg"})
+fs = raw.info["sfreq"]
+
+# %% crop dataset (we only want data for the sleep duration)
+start = datetime(2023, 3, 1, 23, 0, 0, tzinfo=timezone.utc)
+stop = datetime(2023, 3, 2, 6, 0, 0, tzinfo=timezone.utc)
+raw.crop((start - raw.info["meas_date"]).seconds, (stop - raw.info["meas_date"]).seconds)
+
+# %% get ECG time series as 1D NumPy array
+ecg = raw.get_data().squeeze()
+
+# %% detect heartbeats
+beats = sleepecg.detect_heartbeats(ecg, fs)
+sleepecg.plot_ecg(ecg, fs, beats=beats)
+
+# %% load SleepECG classifier (requires tensorflow)
+clf = sleepecg.load_classifier("wrn-gru-mesa", "SleepECG")
+
+# %% predict sleep stages
+record = sleepecg.SleepRecord(
+    sleep_stage_duration=30,
+    recording_start_time=start,
+    heartbeat_times=beats / fs,
+)
+
+stages = sleepecg.stage(clf, record, return_mode="prob")
+
+sleepecg.plot_hypnogram(
+    record,
+    stages,
+    stages_mode=clf.stages_mode,
+    merge_annotations=True,
+)
