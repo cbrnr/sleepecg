600759f616cca9da8276621e11bb77bc235effe9
Clemens Brunner <clemens.brunner@gmail.com>
2024-06-17 12:30:11 +0200

    Fix unused import

--- tests/test_heartbeats.py
+++ tests/test_heartbeats.py
@@ -4,8 +4,6 @@
 
 """Tests for heartbeat detection and detector evaluation."""
 
-from sys import version_info
-
 import numpy as np
 import pytest
 from sleepecg import compare_heartbeats, detect_heartbeats, read_mitdb
