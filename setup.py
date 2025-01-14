2e191cf8741821331b5ec492cc7ec25100a8e88b
Eric Larson <larson.eric.d@gmail.com>
2025-01-14 10:09:56 -0500

    ENH: abi3

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 import numpy as np
 from setuptools import Extension, setup
+from wheel.bdist_wheel import bdist_wheel
+
+# Following https://github.com/joerick/python-abi3-package-sample
+
+
+# Currently set to 3.9. If this is bumped for example to 3.10, the hex
+# in the Extension needs to change to 0x030A0000, and the cibuildwheel `build`
+# selector needs to change in pyproject.toml
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.6
+            return "cp39", "abi3", plat
+
+        return python, abi, plat
+
 
 setup(
     ext_modules=[
@@ -7,6 +26,8 @@ setup(
             "sleepecg._heartbeat_detection",
             ["src/sleepecg/_heartbeat_detection.c"],
             include_dirs=[np.get_include()],
+            define_macros=[("Py_LIMITED_API", "0x03090000")],
+            py_limited_api=True,
         ),
     ],
 )
